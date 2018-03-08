c8f2c4993cb7faa6e9b324a879ac0401072c5c63
Ishu <ishu.k@acapteam.com>
2018-03-08 21:08:03 +0530

    upload code by ishu
    upload code by ishu

--- moneytransferapp/app/scripts/controllers/table.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function tableCtrl() {
-  var vm = this;
-  vm.dataTableOpt = {
-    'ajax': 'data/datatables-arrays.json'
-  };
-}
-angular.module('app').controller('tableCtrl', tableCtrl);
